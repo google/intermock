cc022f58224281a48fa60acd1d9fd21d99231658
Ryan McDermott <ryanmcdermott@google.com>
2018-11-05 08:12:00 -0800

    Fix format check, only work on ts files

--- format-check.sh
+++ format-check.sh
@@ -1,5 +1,6 @@
-#!/bin/bashs
+#!/bin/bash
 git diff HEAD~1 --cached --name-only --diff-filter=ACMRT |
+  grep '.ts$' |
   xargs -n1 clang-format -style=file -output-replacements-xml |
   grep "<replacement " >/dev/null
 if [ $? -ne 1 ]; then 
