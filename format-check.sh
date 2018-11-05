a21a75d009076b7b90cc1aa24b301a5c80cf3435
Ryan McDermott <ryanmcdermott@google.com>
2018-11-05 08:13:21 -0800

    fix format

--- format-check.sh
+++ format-check.sh
@@ -4,6 +4,6 @@ git diff HEAD~1 --cached --name-only --diff-filter=ACMRT |
   xargs -n1 clang-format -style=file -output-replacements-xml |
   grep "<replacement " >/dev/null
 if [ $? -ne 1 ]; then 
-    echo "Commit did not match clang-format"
+    echo "Commit did not match clang-format. Run clang-format over last commit's TS files"
     exit 1
 fi
