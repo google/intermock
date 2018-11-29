03c24803e74ee9b99f20953df548234700d647c9
Ryan McDermott <ryanmcdermott@google.com>
2018-11-29 15:24:14 -0800

    Remove unneeded type

--- src/lang/ts/intermock.ts
+++ src/lang/ts/intermock.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {type} from 'os';
 import ts from 'typescript';
 
 import {DEFAULT_ARRAY_RANGE, FIXED_ARRAY_COUNT} from '../../lib/constants';
